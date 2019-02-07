c37189c7c55b660f25c652e99eaf898bf95ea523
Matyas Cimbulka <2cimbm33@solent.ac.uk>
2019-02-07 22:06:46 +0000

    Creates routes for Rider and Staff resource which returns dummy data for GET requests

--- api/src/main/java/org/io/rideout/resource/StaffResource.java
+++ api/src/main/java/org/io/rideout/resource/StaffResource.java
@@ -1,7 +1,6 @@
 package org.io.rideout.resource;
 
-import org.io.rideout.model.Rider;
-import org.io.rideout.model.Vehicle;
+import org.io.rideout.model.Staff;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import javax.ws.rs.*;
@@ -9,63 +8,57 @@ import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.Date;
 
-@Path("rider")
-public class RiderResource {
+@Path("staff")
+public class StaffResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public ArrayList<Rider> getAllRiders() {
-        ArrayList<Rider> result = new ArrayList<>();
-        Rider dummy = getDummyRider();
-        result.add(dummy);
+    public ArrayList<Staff> getAllStaff() {
+        ArrayList<Staff> result = new ArrayList<>();
+        result.add(getDummyStaff());
         return result;
     }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Rider getRider(@PathParam("id") String id) {
+    public Staff getStaff(@PathParam("id") String id) {
         if (id.equals("12345")) {
-            return getDummyRider();
+            return getDummyStaff();
         }
 
         throw new NotFoundException();
     }
 
-    private Rider getDummyRider() {
-        Rider dummy = new Rider(
+    private Staff getDummyStaff() {
+        return new Staff(
                 "12345",
                 "jsmith",
                 "John",
                 "Smith",
                 new Date(100),
                 "07491012345",
-                "999",
-                true,
-                false,
-                "A"
+                false
         );
-        dummy.addVehicle(new Vehicle("9876", "Honda", "Monkey", 125, "REG123"));
-        return dummy;
     }
 
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
-    public Rider addRider() {
+    public Staff addStaff() {
         throw new NotImplementedException();
     }
 
     @POST
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Rider updateRider(@PathParam("id") String id) {
+    public Staff updateStaff(@PathParam("id") String id) {
         throw new NotImplementedException();
     }
 
     @DELETE
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Rider removeRider(@PathParam("id") String id) {
+    public Staff deleteStaff(@PathParam("id") String id) {
         throw new NotImplementedException();
     }
 }
