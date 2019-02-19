53b78528c91e9a681175554033eaf57a42b21a8f
Matyas Cimbulka <2cimbm33@solent.ac.uk>
2019-02-19 14:05:27 +0000

    Creates a backend for JWT authantication.

---
+++ api/src/main/java/org/io/rideout/authantication/AuthenticationFilter.java
@@ -0,0 +1,69 @@
+package org.io.rideout.authantication;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import javax.annotation.Priority;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+import static javax.ws.rs.Priorities.AUTHENTICATION;
+
+@Secured
+@Provider
+@Priority(AUTHENTICATION)
+public class AuthenticationFilter implements ContainerRequestFilter {
+
+    public static final String REALM = "rideout";
+    public static final String AUTHENTICATION_SCHEMA = "Bearer";
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        String authHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+
+        if (!isTokenBasedAuthentication(authHeader)) {
+            abortWithUnauthorized(requestContext);
+            return;
+        }
+
+        String token = authHeader.substring(AUTHENTICATION_SCHEMA.length()).trim();
+
+        if (!validateToken(token))
+            abortWithUnauthorized(requestContext);
+    }
+
+    private boolean isTokenBasedAuthentication(String authorizationHeader) {
+        return authorizationHeader != null && authorizationHeader.toLowerCase()
+                .startsWith(AUTHENTICATION_SCHEMA.toLowerCase() + " ");
+    }
+
+    private void abortWithUnauthorized(ContainerRequestContext requestContext) {
+        requestContext.abortWith(
+                Response.status(Response.Status.UNAUTHORIZED)
+                        .header(HttpHeaders.WWW_AUTHENTICATE,
+                                AUTHENTICATION_SCHEMA + " realm=\"" + REALM + "\"")
+                        .build()
+        );
+    }
+
+    private boolean validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC512("rideout-secrete-21334243215");
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("rideout")
+                    .build();
+            DecodedJWT jwt = verifier.verify(token);
+            return jwt != null;
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+}
