b3d0f0c617a142cc2cf36c40924273b7f7d2548e
Ed Jeffreys <ed@edjeffreys.com>
2019-02-28 18:44:09 +0000

    Removes nav from app-root
    Also adds landing page button links.
    
    Nav is now explicitly added in components that require nav.
    This means that the landing page is not longer displayed in the nav, and
    login and registration components are not displayed in the nav.

--- frontend/src/app/components/landing/landing.component.ts
+++ frontend/src/app/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    if (this.isLoggedIn()) {
+      this.router.navigateByUrl("dashboard");
+    }
+  }
+
+  isLoggedIn() {
+    return (localStorage.getItem('access_token') !== null)
   }
 
 }
