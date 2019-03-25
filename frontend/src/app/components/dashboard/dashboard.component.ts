9872732d458d141fd87d1001219037897ad19fd8
Ed Jeffreys <ed@edjeffreys.com>
2019-03-25 15:13:29 +0000

    Uses Google MyMaps to display RideOut Thumbnail
    This requires the following:
    * At least one checkpoint to be set in RideOut
    * Route to be set in RideOut (this is the MyMaps map ID)

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {RideOut} from "../../models/rideout";
 import {RideOutService} from "../../services/rideout/ride-out.service";
+import {Checkpoint} from "../../models/checkpoint";
 
 @Component({
   selector: 'app-dashboard',
@@ -23,4 +24,66 @@ export class DashboardComponent implements OnInit {
         this.rideOuts = rideOuts;
       })
   }
+
+  getSWCoord(rideOut: RideOut) {
+    const modifier = 1.8;
+
+    let lat: number;
+    let lon: number;
+
+    rideOut.checkpoints.forEach((checkpoint: Checkpoint) => {
+      if (lat == null) {
+        lat = checkpoint.lat;
+      } else {
+        if (checkpoint.lat < lat) {
+          lat = checkpoint.lat;
+        }
+      }
+      if (lon == null) {
+        lon = checkpoint.lon;
+      } else {
+        if (checkpoint.lon < lon) {
+          lon = checkpoint.lon;
+        }
+      }
+    });
+
+    // Creates a margin for boundary
+    lat -= 0.0054165 * modifier;
+    lon -= 0.1484156 * modifier;
+
+    console.log(`${lat},${lon}`);
+    return `${lat},${lon}`;
+  }
+
+  getNECoord(rideOut: RideOut) {
+    const modifier = 1.8;
+
+    let lat: number;
+    let lon: number;
+
+    rideOut.checkpoints.forEach((checkpoint: Checkpoint) => {
+      if (lat == null) {
+        lat = checkpoint.lat;
+      } else {
+        if (checkpoint.lat > lat) {
+          lat = checkpoint.lat;
+        }
+      }
+      if (lon == null) {
+        lon = checkpoint.lon;
+      } else {
+        if (checkpoint.lon > lon) {
+          lon = checkpoint.lon;
+        }
+      }
+    });
+
+    // Creates a margin for boundary
+    lat += 0.0054165 * modifier;
+    lon += 0.1484156 * modifier;
+
+    return `${lat},${lon}`;
+  }
+
 }
