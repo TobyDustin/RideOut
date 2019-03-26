a359cc9fdb7eac2f374e478a5167c44e4733d084
Ed Jeffreys <ed@edjeffreys.com>
2019-03-26 17:23:10 +0000

    Creates AdminGuard
    * Used to lock down any admin frontend routes to only users who's role
    is 'staff'

---
+++ frontend/src/app/guard/admin.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from "../services/auth/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.auth.isLoggedIn() && this.auth.getRole() == "staff") {
+      return true;
+    }
+    return this.router.parseUrl('/login');
+  }
+  
+}
