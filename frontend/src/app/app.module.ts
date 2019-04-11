3a88aa8467075c03f5c75e86ba3c48e993d1d702
Ed Jeffreys <ed@edjeffreys.com>
2019-04-11 11:46:31 +0100

    Creates user management page
    * Creates table to view all users
    * Creates service to retrieve all users
    * Creates router route
    * Adds link in admin nav bar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { AddRiderInfoComponent } from './components/profile/add-rider-info/add-rider-info.component';
 import { AdminRideoutsComponent } from './components/admin/admin-rideouts/admin-rideouts.component';
 import { AdminNavComponent } from './components/admin/admin-nav/admin-nav.component';
+import { AdminUsersComponent } from './components/admin/admin-users/admin-users.component';
 
 export function tokenGetter() {
   return localStorage.getItem('access_token');
@@ -43,7 +44,8 @@ export function tokenGetter() {
     ProfileComponent,
     AddRiderInfoComponent,
     AdminRideoutsComponent,
-    AdminNavComponent
+    AdminNavComponent,
+    AdminUsersComponent
   ],
   imports: [
     BrowserModule,
