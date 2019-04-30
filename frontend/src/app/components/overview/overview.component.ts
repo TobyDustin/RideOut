e5a07b08d52805774a72c7beb38bce35cb8fed31
Ed Jeffreys <ed@edjeffreys.com>
2019-04-30 10:33:00 +0100

    Allows user to leave rideout after joining

--- frontend/src/app/components/overview/overview.component.ts
+++ frontend/src/app/components/overview/overview.component.ts
@@ -3,9 +3,10 @@ import {ActivatedRoute} from "@angular/router";
 import {RideOut} from "../../models/rideout";
 import {RideOutService} from "../../services/rideout/ride-out.service";
 import {DomSanitizer} from "@angular/platform-browser";
-import {AddVehicleComponent} from "../vehicle/add-vehicle/add-vehicle.component";
-import {MatDialog} from "@angular/material";
+import {MatDialog, MatSnackBar} from "@angular/material";
 import {JoinOverviewComponent} from "./join-overview/join-overview.component";
+import {UserService} from "../../services/user/user.service";
+import {AuthService} from "../../services/auth/auth.service";
 
 @Component({
   selector: 'app-overview',
@@ -17,11 +18,15 @@ export class OverviewComponent implements OnInit {
   constructor(
     private dialog: MatDialog,
     private route: ActivatedRoute,
+    private auth: AuthService,
     private service: RideOutService,
-    private sanitizer: DomSanitizer
+    private userService: UserService,
+    private sanitizer: DomSanitizer,
+    private snackBar: MatSnackBar
   ) { }
 
   public rideOut: RideOut;
+  public userHasJoined: boolean;
 
   ngOnInit() {
     let rideOutId = "";
@@ -34,7 +39,18 @@ export class OverviewComponent implements OnInit {
   getRideOut(rideOutId: string) {
     this.service.getRideOut(rideOutId).subscribe((rideOut) => {
       this.rideOut = rideOut;
+      this.userHasJoined = this.checkUserJoined(rideOut);
     })
+  }
+
+  private checkUserJoined(rideOut: RideOut) : boolean {
+    let riderHasJoined = false;
+    rideOut.riders.forEach((rider) => {
+      if (rider.id == this.auth.getId()) {
+        riderHasJoined = true;
+      }
+    });
+    return riderHasJoined;
   }
 
   getRoute() {
@@ -47,5 +63,15 @@ export class OverviewComponent implements OnInit {
     });
   }
 
+  leaveRideOut() {
+    this.userService.leaveRideOut(this.rideOut.id).subscribe(
+      () => {
+        this.snackBar.open("Left RideOut!")._dismissAfter(5000);
+      },
+      () => {
+        this.snackBar.open("An Error Occurred!")._dismissAfter(5000);
+      }
+    );
+  }
 
 }
